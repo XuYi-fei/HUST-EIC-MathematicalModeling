eac3f1900337bb267b05147e1e47e9eb8004c30c
XuYi-fei <857998989@qq.com>
2021-05-06 19:53:31 +0800

    在svd实验中又加入了噪声重新观察效果

--- Lesson6/L7/q1.m
+++ Lesson6/L7/q1.m
@@ -1,7 +1,7 @@
 clear;
 img = imread("go.jpg");
 %原二值图如下：
-subplot(1,2,1);
+subplot(2,2,1);
 imshow(img);
 
 title("原图");
@@ -9,8 +9,32 @@ img = im2double(img);
 [u,s,v] = svd(img);
 s(s<1) = 0;
 img = u*s*v';
-subplot(1,2,2);
+subplot(2,2,2);
 image(256*img);
 colormap(gray(256));
-title("分解后重构的图形");
+title("分解后重构的图像");
+
+img = imread("go.jpg");
+img = im2double(img);
+row = length(img);
+col = length(img(1,:));
+noise = normrnd(0.05, 0.2, row, col);
+img = img + noise;
+img(img>1) = 1;
+img(img<0) = 0;
+subplot(2,2,3);
+image(256*img);
+colormap(gray(256));
+title("加入噪声后的图像");
+
+[u,s,v] = svd(img);
+s(s<1) = 0;
+img = u*s*v';
+subplot(2,2,4);
+image(256*img);
+colormap(gray(256));
+title("加噪后分解重构的图像");
+
+
+
 
